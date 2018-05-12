605436a50e74b938c1dec6e33acf023406f62155
Luke Leggio <laleggio@ucdavis.edu>
2018-05-12 00:42:18 +0000

    Added basic authentication using passport local strategy. Minor html tweaks

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+var mySqlSessionStore = require('express-mysql-session')(session);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var timesheetRouter = require('./routes/timesheet');
 var componentsRouter = require('./routes/components');
 
 var database = require('./lib/database');
+var auth = require('./lib/authentication');
 
 var app = express();
 
@@ -24,9 +28,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//mysql session setup
+const cookieOptions = {
+    checkExpirationInterval: 1000 * 60 * 15,// 15 min // How frequently expired sessions will be cleared; milliseconds.
+    expiration: 1000 * 60 * 60 * 24,// 1 week // The maximum age of a valid session; milliseconds.
+    createDatabaseTable: true,// Whether or not to create the sessions database table, if one does not already exist.
+    schema: {
+        tableName: 'sessions',
+        columnNames: {
+            session_id: 'session_id',
+            expires: 'expires',
+            data: 'data'
+        }
+    }
+};
+const sessionStore = new mySqlSessionStore(cookieOptions,database.db);
+
+//session setup with mysql store
+app.use(session({
+	secret: 'popsicle',
+	store:sessionStore,
+	saveUninitialized:false,
+	resave:false
+}));
+
+//passport
+var passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/timesheet', timesheetRouter);
+app.use('/timesheet', [auth.isLoggedIn,timesheetRouter]);
 app.use('/components',componentsRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var index = require('../lib/index');
+
+var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index.get);
+
+router.post('/',passport.authenticate('local',{successRedirect:'/timesheet/viewAll',failureRedirect:'/?error=1'}));
+
 
 module.exports = router;
